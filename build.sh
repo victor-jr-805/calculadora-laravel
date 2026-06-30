beaf603eefb622068005ac69effd63843ac9b38b
victor_gabriel_mamani_tancara <vmamani805@gmail.com>
2026-06-30 13:44:07 -0400

    Agregado script de automatizacion para despliegue en RENDER

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 # Salir inmediatamente si un comando falla
 set -o errexit
 
-# Instalar dependencias de PHP y JavaScript
-composer install --no-dev --optimize-autoloader
-npm install
+# 🌟 TRUCO RENDER: Descargar una copia local de Composer en la raíz
+curl -sS https://getcomposer.org/installer | php
 
-# Compilar los archivos de Vite (Tailwind v4 y JS) para producción
+# Instalar dependencias usando la copia local que acabamos de descargar (php composer.phar)
+php composer.phar install --no-dev --optimize-autoloader
+
+# Instalar dependencias de JavaScript y compilar Vite (Tailwind v4)
+npm install
 npm run build
 
-# Limpiar cachés de Laravel para optimizar la velocidad
+# Limpiar cachés de Laravel para optimizar la velocidad en internet
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
